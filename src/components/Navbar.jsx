00430ab5e16287c75dddfe89123011013299e174
Abu Raihan <aburaihancse066@gmail.com>
2024-03-09 22:58:50 +0600

    Category menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex flex-col justify-between py-3 mx-6 lg:flex-row'>
+    <nav className='flex flex-col justify-between py-3 mx-6 mb-10 lg:flex-row'>
       <div>
         <h3 className='text-xl font-bold text-gray-600'>{new Date().toUTCString().slice(0, 16)}</h3>
         <h1 className='text-2xl font-bold py-2'><span className='text-blue-900'>Hungry</span><span className='text-amber-600 bg-lime-500	 py-1 px-2 rounded-xl'>Hub</span></h1>
