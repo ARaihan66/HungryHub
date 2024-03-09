2b2c8f595049e7e341fca04a91b6794417e11a3c
Abu Raihan <aburaihancse066@gmail.com>
2024-03-09 22:10:23 +0600

    Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-col justify-between py-3 mx-6 lg:flex-row'>
+      <div>
+        <h3 className='text-xl font-bold text-gray-600'>{new Date().toUTCString().slice(0, 16)}</h3>
+        <h1 className='text-2xl font-bold py-2'><span className='text-blue-900'>Hungry</span><span className='text-amber-600 bg-lime-500	 py-1 px-2 rounded-xl'>Hub</span></h1>
+      </div>
+      <div>
+        <input type="search" name="search" id="" placeholder='Search Here' autoComplete='off' className='p-3 border border-gray-500 outline-none rounded-lg w-full lg:w-[25vw]' />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <h1 className="text-xl font-bold underline">
+     404 Error
+    </h1>
+  )
+}
+
+export default Error
