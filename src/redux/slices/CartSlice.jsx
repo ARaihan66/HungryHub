40cd319350b326347a3a59ff46d5a714fb137159
Abu Raihan <aburaihancse066@gmail.com>
2024-03-10 12:41:12 +0600

    Cart Item Add Remove Functionality added

---
+++ src/redux/slices/CartSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+export const CartSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {addToCart, removeFromCart} = CartSlice.actions;
+
+export default CartSlice.reducer;
